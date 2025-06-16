f71e481001c2ba188de075dfe3bd3b5d343de255
sakshamsingh03 <sakshamsingh2086@gmail.com>
2025-06-16 10:14:19 +0530

    [Saksham]Add. GreatestFactor file

---
+++ ControlFlow/Level 2/GreatestFactor.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class GreatestFactor {
+
+	public static void main(String[] args) {
+		// Taking the input from the user 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int greatestFactor = 1;
+		int number = input.nextInt();
+		for(int i = number-1; i > 1; i--) {
+			if(number % i == 0) {
+				greatestFactor = i;
+				break;
+			}
+		}
+		System.out.println("Greatest Factor: " + greatestFactor);
+
+	}
+
+}
