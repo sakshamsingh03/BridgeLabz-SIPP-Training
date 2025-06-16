9de2a688e490181ece39cfafef8e886202d4c84c
sakshamsingh03 <sakshamsingh2086@gmail.com>
2025-06-16 09:39:42 +0530

    [Saksham]Add. FactorialUsingForLoop file

---
+++ ControlFlow/Level 1/FactorialUsingForLoop.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class FactorialUsingForLoop {
+
+	public static void main(String[] args) {
+		// Taking the number input from the user 
+		Scanner input = new Scanner(System.in);
+		int factorial = 1;
+		System.out.println("Enter the number: ");
+		int number = input.nextInt();
+		// Checking number is positive or negative
+		if(number > 0) {
+			// Calculating factorial
+			for(int i = number; i > 0; i--) {
+				factorial *= i;
+			}
+			// Printing result
+			System.out.println("Factorial: " + factorial);
+		} else if(number == 0) {
+			System.out.println("Factorial: " + number);
+		}else {
+			System.out.println("The number entered is not positive.");
+		}
+
+	}
+
+}
