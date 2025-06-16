a81b0d82ee3a61d5f75507361323a160551533f1
sakshamsingh03 <sakshamsingh2086@gmail.com>
2025-06-16 10:02:50 +0530

    [Saksham]Add. Factors2 file

---
+++ ControlFlow/Level 2/Factors2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Factors2 {
+
+	public static void main(String[] args) {
+		// Taking the input from the user 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int number = input.nextInt();
+		if(number > 0) {
+			int i = 1;
+			while(number >= i) {
+				if(number % i == 0) {
+					System.out.println(i);
+				}
+				i++;
+			}
+		}
+
+	}
+
+}
