6786c41c4d543b4b5bdb135df819cb5202ccb3fd
sakshamsingh03 <sakshamsingh2086@gmail.com>
2025-06-16 08:36:26 +0530

    [Saksham]Add. divisible by 5 file

---
+++ ControlFlow/Level 1/checkDivisibleByFive.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class checkDivisibleByFive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int number = input.nextInt();
+		boolean state = number % 5 == 0;
+		System.out.println("Is the number " + number + " divisible by 5? " + state);
+
+	}
+
+}
