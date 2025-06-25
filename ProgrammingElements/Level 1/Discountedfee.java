c17255ae0880e0d68d05b04cb64fc5ee334996cc
sakshamsingh03 <sakshamsingh2086@gmail.com>
2025-06-25 09:20:44 +0530

    [Saksham]Fix. cleaning git main branch

--- ProgrammingElements/Level 1/Discountedfee.java
+++
@@ -1,9 +0,0 @@
-class Discountedfee{
-public static void main(String[] args){
-		 int fee = 125000;
-        int percentdiscount = 10;
-        double discount = (fee * percentdiscount) / 100.0;
-        double discountedFee = fee - discount;
-        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + discountedFee);
-}
-}
--- ProgrammingElements/Level 1/Percentageprofit.java
+++
@@ -1,9 +0,0 @@
-class Percentageprofit{
-public static void main(String[] args){
-double cp=129;
-double sp=191;
-double profit= sp-cp;
-double profitpercentage=(profit/cp)*100;
-System.out.print("The Cost Price is INR "+cp+" and selling price is INR "+sp+"The Profit is INR "+profit+"and the Profit Percentage is "+profitpercentage);
-}
-}
