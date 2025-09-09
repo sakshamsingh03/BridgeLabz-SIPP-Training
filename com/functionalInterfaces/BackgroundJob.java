765a68caa09ab96edd85b3077e26200d76e2583f
sakshamsingh03 <sakshamsingh2086@gmail.com>
2025-09-09 16:24:13 +0530

    [Saksham]Add.  Java features

--- com/functionalInterfaces/BackgroundJob.java
+++
@@ -1,8 +0,0 @@
-package com.functionalInterfaces;
-
-public class BackgroundJob {
-    public static void main(String[] args) {
-        Runnable task = () -> System.out.println("Executing background task");
-        new Thread(task).start();
-    }
-}
